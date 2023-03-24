4f0928e3bfb68a2a0c387ae1fc45d8831c2a49c7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-24 12:29:43 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Olympics from "./images/Olympics.jpg"
 import './App.css';
-
+import Header from "./Header/Header";
 function App() {
   return (
     <div className="App">
-     
+     <Header/>
       <img src={Olympics} height={900} width={1300} />
     </div>
   );
